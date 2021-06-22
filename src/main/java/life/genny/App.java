6b75c12a90a9760ccd1a965d276c391358450936
Jian Li <hbjoylee@gmail.com>
2021-06-22 11:05:40 +1000

    temp commit and enable keycloak

--- src/main/java/life/genny/App.java
+++ src/main/java/life/genny/App.java
@@ -1,32 +1,34 @@
 package life.genny;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.lang.invoke.MethodHandles;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import life.genny.bootxport.bootx.*;
-import life.genny.bootxport.utils.HibernateUtil;
 import life.genny.bootxport.xlsimport.BatchLoading;
 
 import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
+@ApplicationScoped
 @Path("/")
 public class App {
-	
-	private static final Logger log = Logger.getLogger(App.class);
+    @Inject
+    EntityManager em;
 
-     private Realm rx;
+
+    private static final Logger log = Logger.getLogger(App.class);
+
     @GET
     @Path("/version")
     @Produces(MediaType.TEXT_PLAIN)
@@ -37,16 +39,12 @@ public class App {
     @GET
     @Path("/loadsheets")
     @Produces(MediaType.TEXT_PLAIN)
-    public void hello() {
+    @Transactional
+    public String loadSheets() {
         Realm realm = new Realm(BatchLoadMode.ONLINE, "17CbqWLICh882xKVTU5J5mqqvGVl2F0Z7mdTgiAHAXx8");
 
         List<Tuple2<RealmUnit, BatchLoading>> collect = realm.getDataUnits().stream().map(d -> {
-                    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-                    Session openSession = sessionFactory.openSession();
-                    EntityManager createEntityManager =
-                            openSession.getEntityManagerFactory().createEntityManager();
-                    QwandaRepository repo =
-                            new QwandaRepositoryImpl(createEntityManager);
+                    QwandaRepository repo = new QwandaRepositoryImpl(em);
                     BatchLoading bl = new BatchLoading(repo);
                     return Tuple.of(d, bl);
                 }
@@ -54,14 +52,15 @@ public class App {
 
         collect.parallelStream().forEach(d ->
                 {
-                    if (!d._1.getDisable() && !d._1.getSkipGoogleDoc())
+                    if (!d._1.getSkipGoogleDoc())
                         d._2.persistProject(d._1);
                     else {
-                        System.out.println("Realm:" + d._1.getName()
+                        log.info("Realm:" + d._1.getName()
                                 + ", disabled:" + d._1.getDisable()
                                 + ", skipGoogleDoc:" + d._1.getSkipGoogleDoc());
                     }
                 }
         );
+        return "Finished batch loading";
     }
 }
