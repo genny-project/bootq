dffc43852709b1466be3d91b0ef47504bc75f2ef
Jian Li <hbjoylee@gmail.com>
2021-07-15 11:10:45 +1000

    udpate DummyEntity

--- src/main/java/life/genny/entity/DummyEntity.java
+++ src/main/java/life/genny/entity/DummyEntity.java
@@ -3,18 +3,31 @@ package life.genny.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class DummyEntity extends PanacheEntity {
-    private Long id;
+    private String name;
+
+    @Id
+    @SequenceGenerator(name = "deSeq", sequenceName = "gift_id_seq", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(generator = "deSeq")
+    public Long getId() {
+        return id;
+    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Id
-    public Long getId() {
-        return id;
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
