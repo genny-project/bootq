f8f7c6ec71eb674e0f88ba910d388e394fa05dc1
Jian Li <hbjoylee@gmail.com>
2021-07-22 15:51:47 +1000

    performance profile

--- src/main/java/life/genny/App.java
+++ src/main/java/life/genny/App.java
@@ -16,13 +16,13 @@ import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import io.vertx.core.json.JsonObject;
 import life.genny.bootxport.bootx.*;
-import life.genny.bootxport.utils.HibernateUtil;
 import life.genny.bootxport.xlsimport.BatchLoading;
 import life.genny.models.GennyToken;
 import life.genny.qwanda.entity.BaseEntity;
 import life.genny.qwanda.entity.SearchEntity;
 import life.genny.qwandautils.GennySettings;
 import life.genny.utils.BaseEntityUtils;
+import life.genny.utils.HibernateUtil;
 import life.genny.utils.RulesUtils;
 import life.genny.utils.VertxUtils;
 
@@ -38,102 +38,113 @@ import org.hibernate.SessionFactory;
 @Path("/bootq/")
 public class App {
 
-	private static final Logger log = Logger.getLogger(App.class);
+    private static final Logger log = Logger.getLogger(App.class);
 
-	@ConfigProperty(name = "quarkus.application.version")
-	String version;
+    @ConfigProperty(name = "quarkus.application.version")
+    String version;
 
-	@GET
-	@Path("/version")
-	@Produces(MediaType.TEXT_PLAIN)
-	public Response version() {
-		return Response.status(200).entity("Application version:" + version).build();
-	}
+    @GET
+    @Path("/version")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response version() {
+        return Response.status(200).entity("Application version:" + version).build();
+    }
 
-	@GET
-	@Path("/loadsheets")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String loadSheets() {
-		String sheetId = System.getenv("GOOGLE_SHEETS_ID");
-		if (StringUtils.isBlank(sheetId)) {
-			sheetId = System.getenv("GOOGLE_HOSTING_SHEET_ID");
-		}
-		loadSheetsById(sheetId);
-		return "Finished batch loading";
-	}
 
-	@GET
-	@Path("/loadsheets/{sheetid}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String loadSheetsById(@PathParam("sheetid") final String sheetId) {
-		String msg = "";
-		if (sheetId == null) {
-			msg = "Can't find env GOOGLE_SHEETS_ID!!!";
-			log.error(msg);
-			return msg;
-		}
+    @GET
+    @Path("/test")
+    public void test() {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session openSession = sessionFactory.openSession();
+        EntityManager createEntityManager = openSession.getEntityManagerFactory().createEntityManager();
+        QwandaRepository repo = new QwandaRepositoryImpl(createEntityManager);
+        BatchLoading bl = new BatchLoading(repo);
+    }
 
-		Realm realm = new Realm(BatchLoadMode.ONLINE, sheetId);
-		List<Tuple2<RealmUnit, BatchLoading>> collect = realm.getDataUnits().stream().map(d -> {
-			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-			Session openSession = sessionFactory.openSession();
-			EntityManager createEntityManager = openSession.getEntityManagerFactory().createEntityManager();
-			QwandaRepository repo = new QwandaRepositoryImpl(createEntityManager);
-			BatchLoading bl = new BatchLoading(repo);
-			return Tuple.of(d, bl);
-		}).collect(Collectors.toList());
+    @GET
+    @Path("/loadsheets")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String loadSheets() {
+        String sheetId = System.getenv("GOOGLE_SHEETS_ID");
+        if (StringUtils.isBlank(sheetId)) {
+            sheetId = System.getenv("GOOGLE_HOSTING_SHEET_ID");
+        }
+        loadSheetsById(sheetId);
+        return "Finished batch loading";
+    }
 
-		collect.parallelStream().forEach(d -> {
-			if (!d._1.getDisable() && !d._1.getSkipGoogleDoc()) {
-				d._2.persistProject(d._1);
-				System.out.println("Finish batch loading, sheetID:" + d._1.getUri());
-			} else {
-				System.out.println("Realm:" + d._1.getName() + ", disabled:" + d._1.getDisable() + ", skipGoogleDoc:"
-						+ d._1.getSkipGoogleDoc());
-			}
-		});
-		return "Finished batch loading";
-	}
+    @GET
+    @Path("/loadsheets/{sheetid}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String loadSheetsById(@PathParam("sheetid") final String sheetId) {
+        String msg = "";
+        if (sheetId == null) {
+            msg = "Can't find env GOOGLE_SHEETS_ID!!!";
+            log.error(msg);
+            return msg;
+        }
 
-	@GET
-	@Path("/loaddefs/{realm}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public Response loadDefs(@PathParam("realm") final String realm) {
+        Realm realm = new Realm(BatchLoadMode.ONLINE, sheetId);
+        List<Tuple2<RealmUnit, BatchLoading>> collect = realm.getDataUnits().stream().map(d -> {
+            SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+            Session openSession = sessionFactory.openSession();
+            EntityManager createEntityManager = openSession.getEntityManagerFactory().createEntityManager();
+            QwandaRepository repo = new QwandaRepositoryImpl(createEntityManager);
+            BatchLoading bl = new BatchLoading(repo);
+            return Tuple.of(d, bl);
+        }).collect(Collectors.toList());
 
-		log.info("Loading in DEFS for realm " + realm);
+        collect.parallelStream().forEach(d -> {
+            if (!d._1.getDisable() && !d._1.getSkipGoogleDoc()) {
+                d._2.persistProject(d._1);
+                System.out.println("Finish batch loading, sheetID:" + d._1.getUri());
+            } else {
+                System.out.println("Realm:" + d._1.getName() + ", disabled:" + d._1.getDisable() + ", skipGoogleDoc:"
+                        + d._1.getSkipGoogleDoc());
+            }
+        });
+        return "Finished batch loading";
+    }
 
-		SearchEntity searchBE = new SearchEntity("SBE_DEF", "DEF test")
-				.addSort("PRI_NAME", "Created", SearchEntity.Sort.ASC)
-				.addFilter("PRI_CODE", SearchEntity.StringFilter.LIKE, "DEF_%")
+    @GET
+    @Path("/loaddefs/{realm}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response loadDefs(@PathParam("realm") final String realm) {
 
-				.addColumn("PRI_NAME", "Name");
+        log.info("Loading in DEFS for realm " + realm);
 
-		searchBE.setRealm(realm);
-		searchBE.setPageStart(0);
-		searchBE.setPageSize(10000);
+        SearchEntity searchBE = new SearchEntity("SBE_DEF", "DEF test")
+                .addSort("PRI_NAME", "Created", SearchEntity.Sort.ASC)
+                .addFilter("PRI_CODE", SearchEntity.StringFilter.LIKE, "DEF_%")
 
-		JsonObject tokenObj = VertxUtils.readCachedJson(GennySettings.GENNY_REALM, "TOKEN" + realm.toUpperCase());
-		String sToken = tokenObj.getString("value");
-		GennyToken serviceToken = new GennyToken("PER_SERVICE", sToken);
+                .addColumn("PRI_NAME", "Name");
 
-		if ((serviceToken == null) || ("DUMMY".equalsIgnoreCase(serviceToken.getToken()))) {
-			log.error("NO SERVICE TOKEN FOR " + realm + " IN CACHE");
-			return Response.status(Status.ERROR).entity("NO SERVICE TOKEN FOR " + realm + " IN CACHE").build();
-		}
-		BaseEntityUtils beUtils = new BaseEntityUtils(serviceToken, serviceToken);
+        searchBE.setRealm(realm);
+        searchBE.setPageStart(0);
+        searchBE.setPageSize(10000);
 
-		List<BaseEntity> items = beUtils.getBaseEntitys(searchBE);
-		log.info("Loaded " + items.size() + " DEF baseentitys");
+        JsonObject tokenObj = VertxUtils.readCachedJson(GennySettings.GENNY_REALM, "TOKEN" + realm.toUpperCase());
+        String sToken = tokenObj.getString("value");
+        GennyToken serviceToken = new GennyToken("PER_SERVICE", sToken);
 
-		RulesUtils.defs.put(realm, new ConcurrentHashMap<String, BaseEntity>());
+        if ((serviceToken == null) || ("DUMMY".equalsIgnoreCase(serviceToken.getToken()))) {
+            log.error("NO SERVICE TOKEN FOR " + realm + " IN CACHE");
+            return Response.status(Status.ERROR).entity("NO SERVICE TOKEN FOR " + realm + " IN CACHE").build();
+        }
+        BaseEntityUtils beUtils = new BaseEntityUtils(serviceToken, serviceToken);
 
-		for (BaseEntity item : items) {
-			item.setFastAttributes(true); // make fast
-			RulesUtils.defs.get(realm).put(item.getCode(), item);
-			log.info("Saving (" + realm + ") DEF " + item.getCode());
-		}
-		log.info("Saved " + items.size() + " yummy DEFs!");
-		return Response.ok().build();
-	}
+        List<BaseEntity> items = beUtils.getBaseEntitys(searchBE);
+        log.info("Loaded " + items.size() + " DEF baseentitys");
+
+        RulesUtils.defs.put(realm, new ConcurrentHashMap<String, BaseEntity>());
+
+        for (BaseEntity item : items) {
+            item.setFastAttributes(true); // make fast
+            RulesUtils.defs.get(realm).put(item.getCode(), item);
+            log.info("Saving (" + realm + ") DEF " + item.getCode());
+        }
+        log.info("Saved " + items.size() + " yummy DEFs!");
+        return Response.ok().build();
+    }
 
 }
