71abb3a18cc0dc4bf00bbd6848f4cc517cdad86a
Bryn Meachem <bryn.meachem@dev-vm-2.gada.local>
2023-02-27 16:58:13 +1100

    removed gettoken-prod.sh

--- gettoken-prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-client_id=alyson
-#/home/ubuntu/scripts/gettoken-prod-script.sh lojing a6197422-8461-4d43-91f0-a8a8c9505c5d alyson "YTNjMmM4ZGY2"
-./gettoken-prod-script.sh internmatch "" lojing "q16Zm7hdw2pjjtdDSkSkBe"
